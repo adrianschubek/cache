46a22e14bea03219f858d52b216e4e759d3d6ce1
adrian <adrian.schubek@gmail.com>
2020-03-15 00:17:18 +0100

    fix

--- src/Cache/Drivers/FileDriverOptions.php
+++ src/Cache/Drivers/FileDriverOptions.php
@@ -11,9 +11,14 @@ class FileDriverOptions
 {
     protected string $cachepath;
 
-    public function setCacheFolder(string $path)
+    public function setCacheFolder(string $path): self
     {
         $this->cachepath = $path;
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        return $this;
     }
 
     public function getCacheFolder(): string
